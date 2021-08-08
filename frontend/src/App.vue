9e9a26830a420a09903b96a8171f591678628980
Thiago Augusto <thiago.digitalfront@gmail.com>
2021-08-08 20:35:35 -0300

    [Frontend] criando estruturade arquivos

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,16 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <HeaderComponent />
     <router-view/>
   </div>
 </template>
+
+<script>
+import HeaderComponent from '@/components/Header.vue'
+export default {
+  name: 'App',
+  components: {
+    HeaderComponent
+  }
+}
+</script>
---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,6 @@
+<template>
+  <div id="nav">
+      <router-link to="/tasks">Tarefas</router-link> |
+      <router-link to="/profile">Perfil</router-link>
+    </div>
+</template>
