0ea01d5b9451ed40e8264f05ee3dacc234f7a566
Thiago Augusto <thiago.digitalfront@gmail.com>
2021-08-08 19:10:53 -0300

    [Backend] factory para criação de usuario inicial

--- backend/database/factories/UserFactory.php
+++ backend/database/factories/UserFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
 class UserFactory extends Factory
@@ -22,13 +23,15 @@ class UserFactory extends Factory
      */
     public function definition()
     {
-        return [
-            'name' => $this->faker->name(),
-            'email' => $this->faker->unique()->safeEmail(),
+        User::create([
+            'name' => 'José da Silva',
+            'email' => 'josedsilva@site.test',
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => Hash::make('password'),
             'remember_token' => Str::random(10),
-        ];
+        ]);
+
+        return [];
     }
 
     /**
