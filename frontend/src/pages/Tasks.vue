218b84b5a1d53802d35e4d527a577369c2e9a159
Thiago Augusto <thiago.digitalfront@gmail.com>
2021-08-08 22:01:11 -0300

    [Frontend] adicionando axios + vue.config

--- frontend/src/pages/Tasks.vue
+++ frontend/src/pages/Tasks.vue
@@ -1,5 +1,42 @@
 <template>
   <div>
     <h1>Pagina de tarefas</h1>
+    <div v-if="loading">
+      carregando
+    </div>
+    <pre v-else>
+      {{ tasks }}
+    </pre>
   </div>
 </template>
+
+<script>
+import API from '@/services/API'
+
+export default {
+  data: function () {
+    return {
+      loading: true,
+      tasks: []
+    }
+  },
+  methods: {
+    loadTasks () {
+      this.loading = true
+      API.get('endpoint')
+        .then(({ data }) => {
+          this.tasks = data.data
+          this.loading = false
+        })
+        .catch(res => {
+          console.log(res)
+          this.loading = false
+        })
+    }
+  },
+  mounted () {
+    console.log('rodando')
+    this.loadTasks()
+  }
+}
+</script>
---
+++ vue.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    // options...
+    devServer: {
+        disableHostCheck: true,
+        host: 'localhost',
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
+        },
+        watchOptions: {
+            poll: true
+        },
+        proxy: 'http://localhost/api',
+    } 
+}
